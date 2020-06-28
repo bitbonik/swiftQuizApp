6ce72b3becc64aaea64820bb35ec1b84e2cf04a8
Rajibul Islam <4850630-rajibislam@users.noreply.gitlab.com>
2020-06-28 14:28:08 +0600

    Initial Commit

---
+++ QuizApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  QuizApp
+//
+//  Created by Rajib on 28/6/20.
+//  Copyright © 2020 Rajib. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
